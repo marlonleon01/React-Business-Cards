bc642a868e1f4683068b50c0ee7132fc07c4ccd6
Marlon Leon <ghostveene@gmail.com>
2023-01-03 13:08:11 -0500

    added className to elements and info to file

--- src/Info.js
+++ src/Info.js
@@ -2,15 +2,19 @@ import Maya from "./imgs/Maya photo.jpg"
 
 export function Info() {
     return (
-        <div>
+        <div className="info">
             {/* Add professsional picture */}
-            <img src={Maya}></img>
-            <h1>Marlon Leon</h1>
-            <h2>Frontend Developer</h2>
+            <img src={Maya} className="professional-img"></img>
+            <h1 className="name">Marlon Leon</h1>
+            <h2 className="occupation">Frontend Developer</h2>
             {/* Add real website link here when you create it */}
-            <h3>Fakewebsitelink.com</h3>
-            <button className="email-btn"><i class="fa-solid fa-envelope"></i>Email</button>
-            <button className="linkedln-btn"><i class="fa-brands fa-linkedin"></i>Linkedln</button>
+            <h3 className="website-link">Fakewebsitelink.com</h3>
+            <button className="email-btn">
+                <i class="fa-solid fa-envelope" className="email-icon"></i>Email
+            </button>
+            <button className="linkedln-btn">
+                <i class="fa-brands fa-linkedin" className="linkedln-icon"></i>Linkedln
+            </button>
         </div>
     )
 }

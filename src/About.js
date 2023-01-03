ab07525ed42c828eafaa589d905be4e7692c0ded
Marlon Leon <ghostveene@gmail.com>
2023-01-03 15:27:16 -0500

    took p elements out of h2 elements

--- src/About.js
+++ src/About.js
@@ -1,18 +1,16 @@
 export function About() {
     return (
         <div className="main-content-about">
-            <h2 className="about-heading">About
-                <p className="about-text">I am an aspiring front-end developer that 
+            <h2 className="about-heading">About</h2>
+            <p className="about-text">I am an aspiring front-end developer that 
                 has always loved solving problems. I make things simple and readable 
                 by all and ensure my apps are bug-free. I still have a long way to go, 
                 and I am working hard to get there.
-                </p>
-            </h2>
-            <h2 className="interests-heading">Interests
-                <p className="interests-text">Musician. Internet fanatic. 
+            </p>
+            <h2 className="interests-heading">Interests</h2>
+            <p className="interests-text">Musician. Internet fanatic. 
                 Adrenaline junkie. Gym rat. Cars and Motorcycles. Food lover. 
-                </p>
-            </h2>
+            </p>
         </div>
     ) 
 }

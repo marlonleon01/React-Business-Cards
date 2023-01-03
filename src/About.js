b761a0598f927fc93d85163e8761e61a83975870
Marlon Leon <ghostveene@gmail.com>
2023-01-03 15:15:59 -0500

    added all content and classNames

--- src/About.js
+++ src/About.js
@@ -1,3 +1,18 @@
 export function About() {
-    
+    return (
+        <div className="main-content-about">
+            <h2 className="about-heading">About
+                <p className="about-text">I am an aspiring front-end developer that 
+                has always loved solving problems. I make things simple and readable 
+                by all and ensure my apps are bug-free. I still have a long way to go, 
+                and I am working hard to get there.
+                </p>
+            </h2>
+            <h2 className="interests-heading">Interests
+                <p className="interests-text">Musician. Internet fanatic. 
+                Adrenaline junkie. Gym rat. Cars and Motorcycles. Food lover. 
+                </p>
+            </h2>
+        </div>
+    ) 
 }

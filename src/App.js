48df54e4777532235a01560a0e549897b70aec2b
Marlon Leon <ghostveene@gmail.com>
2023-01-03 12:43:04 -0500

    connected all components to app.js and then connected app.js to index.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Info } from "./Info";
+import { About } from "./About";
+import { Interests } from "./Interests";
+import { Footer } from "./Footer";
+
+export function App() {
+    return(
+        <div>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </div>
+    )
+}

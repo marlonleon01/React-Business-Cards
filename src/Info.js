ff86b3bea23c73a6cef19dbf21918fac13c45d5e
Marlon Leon <ghostveene@gmail.com>
2023-01-03 13:04:34 -0500

    added all jsx need to style in css

--- src/Info.js
+++ src/Info.js
@@ -1,3 +1,16 @@
+import Maya from "./imgs/Maya photo.jpg"
+
 export function Info() {
-    
+    return (
+        <div>
+            {/* Add professsional picture */}
+            <img src={Maya}></img>
+            <h1>Marlon Leon</h1>
+            <h2>Frontend Developer</h2>
+            {/* Add real website link here when you create it */}
+            <h3>Fakewebsitelink.com</h3>
+            <button className="email-btn"><i class="fa-solid fa-envelope"></i>Email</button>
+            <button className="linkedln-btn"><i class="fa-brands fa-linkedin"></i>Linkedln</button>
+        </div>
+    )
 }

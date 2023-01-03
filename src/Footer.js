6c0dffd6ea05cf735953b1c6777aab0be7337db5
Marlon Leon <ghostveene@gmail.com>
2023-01-03 15:57:41 -0500

    added className to footer and icons

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,18 @@ import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Footer() {
     return (
-        <footer>
-            <a href="https://www.facebook.com/marlon.leon.710" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faSquareFacebook}></FontAwesomeIcon>
+        <footer className="footer">
+            <a href="https://www.facebook.com/marlon.leon.710" target="_blank" 
+            rel="noopener noreferrer" className="anchor-facebook">
+                <FontAwesomeIcon icon={faSquareFacebook} className="facebook-icon"></FontAwesomeIcon>
             </a>
-            <a href="https://www.instagram.com/marlon.z32/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
+            <a href="https://www.instagram.com/marlon.z32/" target="_blank" 
+            rel="noopener noreferrer" className="anchor-instagram">
+                <FontAwesomeIcon icon={faInstagramSquare} className="instagram-icon"></FontAwesomeIcon>
             </a>
-            <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
+            <a href="https://github.com/marlonleon01" target="_blank" 
+            rel="noopener noreferrer" className="anchor-github">
+                <FontAwesomeIcon icon={faGithubSquare} className="github-icon"></FontAwesomeIcon>
             </a>
         </footer>
     )

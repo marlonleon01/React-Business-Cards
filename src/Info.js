f5d4391c398e05ed919cab4c26921c77b126f643
Marlon Leon <ghostveene@gmail.com>
2023-01-03 14:17:18 -0500

    added div called info, removed icons need to readd

--- src/Info.js
+++ src/Info.js
@@ -1,20 +1,25 @@
 import Maya from "./imgs/Maya photo.jpg"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
+import {falink} from "@fortawesome/free-solid-svg-icons"
 
 export function Info() {
     return (
-        <div className="info">
+        <div>
             {/* Add professsional picture */}
             <img src={Maya} className="professional-img"></img>
-            <h1 className="name">Marlon Leon</h1>
-            <h2 className="occupation">Frontend Developer</h2>
-            {/* Add real website link here when you create it */}
-            <h3 className="website-link">Fakewebsitelink.com</h3>
-            <button className="email-btn">
-                <i class="fa-solid fa-envelope" className="email-icon"></i>Email
-            </button>
-            <button className="linkedln-btn">
-                <i class="fa-brands fa-linkedin" className="linkedln-icon"></i>Linkedln
-            </button>
+            <div className="info">
+                <h1 className="name">Marlon Leon</h1>
+                <h2 className="occupation">Frontend Developer</h2>
+                {/* Add real website link here when you create it */}
+                <h3 className="website-link">Fakewebsitelink.com</h3>
+                <button className="email-btn">
+                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>Email
+                </button>
+                <button className="linkedin-btn">
+                    LinkedIn
+                </button>
+            </div>
         </div>
     )
 }
